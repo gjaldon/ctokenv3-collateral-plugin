fad0ce03f9bcacb1e4d3f26a21c354892f1e2a74
Gabriel Jaldon <gjaldon85@gmail.com>
2022-10-28 11:30:40 +0800

    Initial commit

---
+++ contracts/ICollateral.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+/**
+ * @title IAsset
+ * @notice Supertype. Any token that interacts with our system must be wrapped in an asset,
+ * whether it is used as RToken backing or not. Any token that can report a price in the UoA
+ * is eligible to be an asset.
+ */
+interface IAsset {
+    /// Can return 0, can revert
+    /// Shortcut for price(false)
+    /// @return {UoA/tok} The current price(), without considering fallback prices
+    function strictPrice() external view returns (uint192);
+
+    /// Can return 0
+    /// Should not revert if `allowFallback` is true. Can revert if false.
+    /// @param allowFallback Whether to try the fallback price in case precise price reverts
+    /// @return isFallback If the price is a failover price
+    /// @return {UoA/tok} The current price(), or if it's reverting, a fallback price
+    function price(bool allowFallback) external view returns (bool isFallback, uint192);
+
+    /// @return {tok} The balance of the ERC20 in whole tokens
+    function bal(address account) external view returns (uint192);
+
+    /// @return The ERC20 contract of the token with decimals() available
+    function erc20() external view returns (IERC20Metadata);
+
+    /// @return The number of decimals in the ERC20; just for gas optimization
+    function erc20Decimals() external view returns (uint8);
+
+    /// @return If the asset is an instance of ICollateral or not
+    function isCollateral() external view returns (bool);
+
+    /// @param {UoA} The max trade volume, in UoA
+    function maxTradeVolume() external view returns (uint192);
+
+    // ==== Rewards ====
+
+    /// Get the message needed to call in order to claim rewards for holding this asset.
+    /// Returns zero values if there is no reward function to call.
+    /// @return _to The address to send the call to
+    /// @return _calldata The calldata to send
+    function getClaimCalldata() external view returns (address _to, bytes memory _calldata);
+
+    /// The ERC20 token address that this Asset's rewards are paid in.
+    /// If there are no rewards, will return a zero value.
+    function rewardERC20() external view returns (IERC20 reward);
+}
+
+/// CollateralStatus must obey a linear ordering. That is:
+/// - being DISABLED is worse than being IFFY, or SOUND
+/// - being IFFY is worse than being SOUND.
+enum CollateralStatus {
+    SOUND,
+    IFFY, // When a peg is not holding or a chainlink feed is stale
+    DISABLED // When the collateral has completely defaulted
+}
+
+/**
+ * @title ICollateral
+ * @notice A subtype of Asset that consists of the tokens eligible to back the RToken.
+ */
+interface ICollateral is IAsset {
+    /// Refresh exchange rates and update default status.
+    /// The Reserve protocol calls this at least once per transaction, before relying on
+    /// this collateral's prices or default status.
+    function refresh() external;
+
+    /// @return The canonical name of this collateral's target unit.
+    function targetName() external view returns (bytes32);
+
+    /// @return The status of this collateral asset. (Is it defaulting? Might it soon?)
+    function status() external view returns (CollateralStatus);
+
+    // ==== Exchange Rates ====
+
+    /// @return {ref/tok} Quantity of whole reference units per whole collateral tokens
+    function refPerTok() external view returns (uint192);
+
+    /// @return {target/ref} Quantity of whole target units per whole reference unit in the peg
+    function targetPerRef() external view returns (uint192);
+
+    /// @return {UoA/target} The price of the target unit in UoA (usually this is {UoA/UoA} = 1)
+    function pricePerTarget() external view returns (uint192);
+}
