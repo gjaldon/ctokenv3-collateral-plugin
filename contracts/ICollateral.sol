2365199a23d5fd3bcebec240085d9a6c94870301
Gabriel Jaldon <gjaldon85@gmail.com>
2022-10-29 10:56:06 +0800

    Collateral interface

--- contracts/ICollateral.sol
+++ contracts/ICollateral.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
 /**
  * @title IAsset
  * @notice Supertype. Any token that interacts with our system must be wrapped in an asset,
