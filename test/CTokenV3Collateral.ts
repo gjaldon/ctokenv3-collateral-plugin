c8bb812ea4b936206468597c2d0fae44ae75117b
Gabriel Jaldon <gjaldon85@gmail.com>
2022-11-04 12:12:00 +0800

    Use bigInt instead of BigNumber in tests

--- test/CTokenV3Collateral.ts
+++ test/CTokenV3Collateral.ts
@@ -4,18 +4,14 @@ import { ContractFactory, BigNumber } from 'ethers'
 import { OracleLib, MockV3Aggregator } from '../typechain-types'
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
 
-export const pow10 = (exponent: number): BigNumber => {
-  return BigNumber.from(10).pow(exponent)
-}
-
 const USDCtoUSDPriceFeedAddr = '0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6'
 const cUSDCv3Addr = '0xc3d688B66703497DAA19211EEdff47f25384cdc3'
 const compAddr = '0xc00e94Cb662C3520282E6f5717214004A7f26888'
 const rewardsAddr = '0x1B0e765F6224C21223AeA2af16c1C46E38885a40'
-const ORACLE_TIMEOUT = BigNumber.from('281474976710655').div(2) // type(uint48).max / 2
-const DEFAULT_THRESHOLD = BigNumber.from(5).mul(pow10(16))
-const DELAY_UNTIL_DEFAULT = BigNumber.from('86400')
-const rTokenMaxTradeVolume = BigNumber.from(1000000)
+const ORACLE_TIMEOUT = 281474976710655n / 2n // type(uint48).max / 2
+const DEFAULT_THRESHOLD = 5n * 10n ** 16n // 0.05
+const DELAY_UNTIL_DEFAULT = 86400n
+const rTokenMaxTradeVolume = 1000000n
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 const USDC_DECIMALS = 6
 
@@ -184,9 +180,9 @@ describe('Prices #fast', () => {
     const { collateral, chainlinkFeed } = await loadFixture(deployCollateral)
     const { answer } = await chainlinkFeed.latestRoundData()
     const decimals = await chainlinkFeed.decimals()
+    const expectedPrice = answer.toBigInt() * 10n ** BigInt(18 - decimals)
 
     // Check initial prices
-    const expectedPrice = answer.mul(pow10(18 - decimals))
     expect(await collateral.strictPrice()).to.equal(expectedPrice)
 
     // Check refPerTok initial values
@@ -194,12 +190,12 @@ describe('Prices #fast', () => {
     expect(await collateral.refPerTok()).to.equal(expectedRefPerTok) // should equal 1e18
 
     // Update values in Oracles increase by 10-20%
-    const newPrice = BigNumber.from(11).mul(pow10(7))
+    const newPrice = 11n * 10n ** 7n
     const updateAnswerTx = await chainlinkFeed.updateAnswer(newPrice)
     await updateAnswerTx.wait()
 
     // Check new prices
-    expect(await collateral.strictPrice()).to.equal(newPrice.mul(pow10(18 - decimals)))
+    expect(await collateral.strictPrice()).to.equal(newPrice * 10n ** BigInt(18 - decimals))
 
     // Check refPerTok remains the same
     expect(await collateral.refPerTok()).to.equal(expectedRefPerTok)
