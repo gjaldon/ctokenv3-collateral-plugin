4ffe751ba122f1ff0a57a85386563e17cab354f2
Gabriel Jaldon <gjaldon85@gmail.com>
2022-11-08 17:44:11 +0800

    Prepare Reserve artifacts for integration tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import '@typechain/hardhat'
 import '@nomiclabs/hardhat-ethers'
 import '@nomicfoundation/hardhat-chai-matchers'
 import '@nomicfoundation/hardhat-toolbox'
+import 'hardhat-dependency-compiler'
 import { HardhatUserConfig } from 'hardhat/config'
 import dotenv from 'dotenv'
 
@@ -30,6 +31,31 @@ const config: HardhatUserConfig = {
         version: '0.6.12',
         settings: { optimizer: { enabled: false } },
       },
+    ],
+  },
+  dependencyCompiler: {
+    paths: [
+      'reserve/contracts/plugins/assets/Asset.sol',
+      'reserve/contracts/plugins/trading/GnosisTrade.sol',
+      'reserve/contracts/plugins/mocks/ERC20Mock.sol',
+      'reserve/contracts/plugins/mocks/GnosisMock.sol',
+      'reserve/contracts/plugins/mocks/EasyAuction.sol',
+      'reserve/contracts/plugins/mocks/ChainlinkMock.sol',
+      'reserve/contracts/p1/Main.sol',
+      'reserve/contracts/p1/mixins/RewardableLib.sol',
+      'reserve/contracts/p1/mixins/RecollateralizationLib.sol',
+      'reserve/contracts/p1/AssetRegistry.sol',
+      'reserve/contracts/p1/BackingManager.sol',
+      'reserve/contracts/p1/BasketHandler.sol',
+      'reserve/contracts/p1/Broker.sol',
+      'reserve/contracts/p1/Deployer.sol',
+      'reserve/contracts/p1/Distributor.sol',
+      'reserve/contracts/p1/Furnace.sol',
+      'reserve/contracts/p1/RevenueTrader.sol',
+      'reserve/contracts/p1/RToken.sol',
+      'reserve/contracts/p1/StRSR.sol',
+      'reserve/contracts/p1/StRSRVotes.sol',
+      'reserve/contracts/libraries/Permit.sol',
     ],
   },
 }
