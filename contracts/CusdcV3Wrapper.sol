34e321001beca210229d64b9f6902667400f4f3e
Gabriel Jaldon <gjaldon85@gmail.com>
2022-11-15 15:51:26 +0800

    Scale computations for fractions

--- contracts/CusdcV3Wrapper.sol
+++ contracts/CusdcV3Wrapper.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract CusdcV3Wrapper is ERC20 {
   IERC20 public immutable underlying;
+  uint constant expScale = 1e18;
 
   constructor(IERC20 cusdcv3) ERC20("Wrapped CUSDCV3", "wCUSDCv3") {
     underlying = cusdcv3;
@@ -34,25 +35,35 @@ contract CusdcV3Wrapper is ERC20 {
    */
   function withdrawTo(address account, uint256 amount) public virtual returns (bool) {
       uint256 underlyingBalance = underlyingBalanceOf(account);
+      uint256 _underlyingExchangeRate = underlyingExchangeRate();
+      uint256 burnAmount;
 
       if (amount > underlyingBalance) {
+        burnAmount = underlyingBalance * expScale / _underlyingExchangeRate;
+        _burn(_msgSender(), burnAmount);
         SafeERC20.safeTransfer(underlying, account, underlyingBalance);
       } else {
+        burnAmount = amount * expScale / _underlyingExchangeRate;
+        _burn(_msgSender(), burnAmount);
         SafeERC20.safeTransfer(underlying, account, amount);
       }
-
-      uint256 burnAmount = amount / underlyingExchangeRate();
-      _burn(_msgSender(), burnAmount);
 
       return true;
   }
 
   function underlyingBalanceOf(address account) public view returns (uint256) {
     uint256 wrappedTokenAmount = balanceOf(account);
-    return wrappedTokenAmount * underlyingExchangeRate();
+    if (wrappedTokenAmount == 0) {
+      return 0;
+    }
+    return wrappedTokenAmount * underlyingExchangeRate() / expScale;
   }
 
   function underlyingExchangeRate() public view returns (uint256) {
-    return underlying.balanceOf(address(this)) / totalSupply();
+    uint256 totalSupply = totalSupply();
+    if (totalSupply == 0) {
+      return 0;
+    }
+    return underlying.balanceOf(address(this)) * expScale / totalSupply;
   }
 }
